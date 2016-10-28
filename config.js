ebe7a518438ceea67fb0611a4af18d85eb2b5177
Yording <yording9@gmail.com>
2016-10-28 16:46:31 -0500

    refactor

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost/shop'
+	db: process.env.MONGODB || 'mongodb://wedevjs:wedevjs99@ds047166.mlab.com:47166/portfolio'
 };
