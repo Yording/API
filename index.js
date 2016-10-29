f866e5cd762d32430779ad5344c206365cd51d00
Yording <yording9@gmail.com>
2016-10-28 23:17:41 -0500

    Cambios

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const config = require('./config');
 const app = require("./app");
 
 
-mongoose.connect(config.db);
+mongoose.connect(config.db, (err,res) =>{
+	if(err) throw err;
+	console.log("Conexión a la base de datos establecida...");
 
-app.listen(config.port);
+	app.listen(config.port,() => {
+		console.log(`Api REST http://localhost:${config.port}`);
+	});
+});
+
